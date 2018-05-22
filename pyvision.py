4308bb9ce411b3de32a33be2815efa8f067d9ff0
Paul Crooks <pgcrooks@gmail.com>
2018-05-22 12:49:59 +0100

    Adding file download via requests

--- pyvision.py
+++ pyvision.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
 
+import argparse
 import io
 import os
 import re
+import requests
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
 
 
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
+
+
 def get_string_from_screen(placeholder, input):
     """
-    Foo
+    Finds a string by placeholder on an image and returns it.
+    Example:
+      IP address: <bit_we_want>
     """
     return re.search('{}: (.*)'.format(placeholder), input).group(1)
 
 
-def main():
+def main(image_path):
     """
     Main method.
     """
@@ -24,10 +40,11 @@ def main():
     # Instantiates a client
     client = vision.ImageAnnotatorClient()
 
-    # The name of the image file to annotate
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        '/home/pcrooks/Pictures/admin.png')
+    # If the image path is a URL, get it
+    if not os.path.isfile(image_path):
+        file_name = download_file(image_path)
+    else:
+        file_name = os.path.join(os.path.dirname(__file__), image_path)
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
@@ -37,13 +54,13 @@ def main():
 
     # Performs label detection on the image file
     response = client.document_text_detection(image=image)
-    admin_text = response.text_annotations[0].description
+    screen_text = response.text_annotations[0].description
     print('Found:')
-    print(admin_text)
+    print(screen_text)
 
-    ip_address = get_string_from_screen('IP address', admin_text)
-    serial_number = get_string_from_screen('Serial number', admin_text)
-    hostname = get_string_from_screen('Hostname', admin_text)
+    ip_address = get_string_from_screen('IP address', screen_text)
+    serial_number = get_string_from_screen('Serial number', screen_text)
+    hostname = get_string_from_screen('Hostname', screen_text)
     print('IP: {}'.format(ip_address))
     print('Hostname: {}'.format(hostname))
     print('Serial: {}'.format(serial_number))
@@ -51,7 +68,12 @@ def main():
 
 if __name__ == "__main__":
     # Execute only if run as a script
+
+    parser = argparse.ArgumentParser(description='Parse text out of images')
+    parser.add_argument('-i', '--image', required=True, help="Image file or URL")
+    args = parser.parse_args()
+
     try:
-        main()
+        main(args.image)
     except Exception as e:
         print(e)
